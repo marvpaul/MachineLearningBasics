aaa2b3ccac2f7c01de86596e32d13be8c3d13ab9
marvpaul <marvin.krueger@yahoo.de>
2018-05-16 13:29:25 +0200

    Added task 3.4

--- LogisticRegression/beleg3.py
+++ LogisticRegression/beleg3.py
@@ -70,15 +70,44 @@ print(h(np.array([[1, 2], [1, 2]])))
 def cross_entropy_loss(X, y):
     def loss(theta):
         h = logistic_hypothesis(theta)
-        pos_values = y * np.log(h(X))
+        pos_values = -y * np.log(h(X))
         neg_values = np.log(1-h(X))
-        sum_cross_entr = np.sum(pos_values + neg_values)
-        return -1 / len(X) * sum_cross_entr
+        sum_cross_entr = pos_values - neg_values
+        return sum_cross_entr
     return loss
 
 def squared_error_loss(h, X, y):
-    return 1 / (2*len(X)) * np.sum(np.square(h(X)-y))
-
-print(cross_entropy_loss(x, h(x))(np.array([100, 10, -100])))
+    return 1 / 2 * np.square(h(X)-y)
+loss = cross_entropy_loss(x, h(x))
+print(loss(np.array([100, 10, -100])))
 
 print(cross_entropy_loss(x, h(x))(theta))
+
+
+# 4) Implementieren Sie die Kostenfunktion J als Python Funktion:
+# cost_function(X, y, h, loss)
+# zusätzlich zu X und y soll die Funktion die Hypothese h
+# und den Loss aufnehmen.
+#
+# Die Pythonfunktion soll dabei eine Funktion zurückgeben, die
+# den Parametervektor theta aufnimmt.
+
+def cost_function(X, y, h, loss):
+    def costs(theta):
+        loss_computed = loss(theta)
+        return 1/len(X) * np.sum(loss_computed)
+    return costs
+
+print("Costs for good theta", cost_function(x, h(x), h, loss)(theta))
+print("Costs for bad theta", cost_function(x, h(x), h, loss)(np.array([100, 10, -100])))
+
+# 5) Implementieren Sie das Gradientenabstiegsverfahren unter Benutzung der Kostenfunktion und der Hypothese.
+# 5a) Schreiben Sie eine Funktion die die Update Rules anwendet zur Berechnung der neuen theta-Werte:
+# theta = compute_new_theta(x, y, theta, alpha, hypothesis)
+# 5b) Wählen Sie Startwerte in der Umgebung des Miniums der Kostenfunktion für theta.
+# Wenden Sie iterativ die compute_new_theta Funktion an und finden Sie so ein Theta mit niedrigen Kosten.
+# Kapseln Sie dies in eine Funktion:
+# theta = gradient_descent(alpha, theta, nb_iterations, X, y)
+
+# 6) Zeichen Sie die Entscheidungsebene in den Scatter-Plot der Daten
+# Hinweis: Für diese gilt: theta[0] + theta[1] * x1 + theta[2] * x2 = 0.5
