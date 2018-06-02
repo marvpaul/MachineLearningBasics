46e6737e55aac862743e6e9f55dbd21a3b0a06ca
marvpaul <marvin.krueger@yahoo.de>
2018-06-02 13:03:27 +0200

    Added some plots for average hypothesis

--- LogisticRegression/beleg4.py
+++ LogisticRegression/beleg4.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def target_fun(x):
     return np.sin(x)
@@ -13,7 +13,7 @@ def linear_hypothesis_h1(theta_0, theta_1):
 
 def linear_hypothesis_h0(theta_0):
     def fct(x):
-        return theta_0
+        return np.ones(shape=x.shape) * theta_0
 
     return fct
 
@@ -42,7 +42,7 @@ rand_values = np.random.rand(2*10000)*2*np.pi
 fun_values = target_fun(rand_values)
 two_samples_for_each = np.array((rand_values, fun_values)).transpose().reshape((10000, 2, 2))
 print(get_h1([[-2, -4], [0, 1]])[0](-1))
-print(get_h0([[-2, 0], [0, 2]])(10))
+print(get_h0([[-2, 0], [0, 2]])(np.array(10)))
 
 #Get some training data
 rand_values = np.random.rand(100*10000)*2*np.pi
@@ -71,11 +71,29 @@ for i in range(len(hypothesis_h0)):
     e_out_h1[i] = (get_loss(hypothesis_h1[i][0], twenty_train_for_each[i]))
     e_out_h0[i] = (get_loss(hypothesis_h0[i], twenty_train_for_each[i]))
 
+def plot_hypothesis(hypothesis_h0, hypothesis_h1):
+    x = np.arange(0, 2*np.pi, 0.1)
+    y_0 = hypothesis_h0(x)
+    y_1 = hypothesis_h1(x)
+    sin = np.sin(x)
+    fig, ax = plt.subplots()
+    ax.plot(x, y_0)
+    ax.plot(x, y_1)
+    ax.plot(x, sin)
+    plt.show()
+
+
 average_h0_hypthesis = 0
 
 for hypothessis in hypothesis_h0:
-    average_h0_hypthesis += hypothessis(100)
-print(average_h0_hypthesis/len(hypothesis_h0))
+    average_h0_hypthesis += hypothessis(np.array(100))
+
+average_h0_hypthesis = average_h0_hypthesis/len(hypothesis_h0)
+print("========Hypothesis 0==========")
+print("theta0 -->", average_h0_hypthesis)
+
+
+
 
 average_theta0 = 0
 average_theta1 = 0
@@ -83,8 +101,10 @@ for hypothessis in hypothesis_h1:
     average_theta0 += hypothessis[1][0]
     average_theta1 += hypothessis[1][1]
 
-print("n", average_theta0/len(hypothesis_h0))
+average_theta0 = average_theta0/len(hypothesis_h0)
+average_theta1 = average_theta1/len(hypothesis_h0)
+print("========Hypothesis 1==========")
+print("theta0 -->", average_theta0)
+print("theta1 -->", average_theta1)
 
-print("m", average_theta1/len(hypothesis_h0))
-
-print("Sth")
+plot_hypothesis(linear_hypothesis_h0(average_h0_hypthesis), linear_hypothesis_h1(average_theta0, average_theta1))
